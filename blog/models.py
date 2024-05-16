e6c770e3e8ea644a2ec7ba180501160e166be6c4
Olle Brask <ollstah@gmail.com>
2024-05-16 12:01:14 +0000

    Fix typo Charfield to CharField

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class GolfCourse(models.Model):
     name = models.CharField(max_length=50)
-    location = models.Charfield(max_length=50)
+    location = models.CharField(max_length=50)
     holes_amount = models.IntegerField()
     extra_facilities = models.TextField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
