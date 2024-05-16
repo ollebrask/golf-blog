0fa75396ecb00d8f9efd10435d4e090566ed1347
Olle Brask <ollstah@gmail.com>
2024-05-16 09:52:41 +0000

    Create Review model

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,16 @@ class GolfCourse(models.Model):
     def __str__(self):
         return self.name
 
+class Review(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    golf_course = models.ForeignKey(GolfCourse, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating =  models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
+
+
