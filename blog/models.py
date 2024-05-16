76aa728ea0129c9ad2df230eea80d6ab8b2eaf2d
Olle Brask <ollstah@gmail.com>
2024-05-16 09:35:40 +0000

    Create GolfCourse model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GolfCourse(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.Charfield(max_length=50)
+    holes_amount = models.IntegerField()
+    extra_facilities = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
