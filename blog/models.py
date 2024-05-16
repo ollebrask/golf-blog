a70e80330157dc54be49aae21c98f6adf285da7e
Olle Brask <ollstah@gmail.com>
2024-05-16 12:00:02 +0000

    Create Comment model

--- blog/models.py
+++ blog/models.py
@@ -27,4 +27,14 @@ class Review(models.Model):
     def __str__(self):
         return f"{self.title} by {self.user.username}"
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.review.title}"
+
+
 
