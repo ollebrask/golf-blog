2bef0e0227e636116635de3d0fb44940c9241d9b
Olle Brask <ollstah@gmail.com>
2024-05-20 12:35:28 +0000

    Add django summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import GolfCourse, Review, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
 admin.site.register(GolfCourse)
-admin.site.register(Review)
 admin.site.register(Comment)
