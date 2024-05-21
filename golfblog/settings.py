a3ab6784e5a149955e23a8057c7c7ad8093b5138
Olle Brask <ollstah@gmail.com>
2024-05-21 11:02:18 +0000

    Add static

--- golfblog/settings.py
+++ golfblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-4d2#&140#f5u9ny$fus!p!5)d1%$1*_^$5lwukuw+w^pb1)x3o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ollebrask-golfblog-lxl5zvvq3yq.ws-eu111.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ollebrask-golfblog-lxl5zvvq3yq.ws-eu114.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
