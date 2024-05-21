27bd7ac9c4ee2320de9d7430651f7ec3b76e444b
Olle Brask <ollstah@gmail.com>
2024-05-21 11:01:52 +0000

    Add semicolon to make sure its shown

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Review
 
 # Create your views here.
 def index(request):
-    latest_reviews = Review.objects.order_by('-created_on')[3]
+    latest_reviews = Review.objects.order_by('-created_on')[:3]
     return render(request, 'blog/index.html', {'latest_reviews': latest_reviews})
