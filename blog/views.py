246031cdb8f39bb9785163ce3281932310420ef0
Olle Brask <ollstah@gmail.com>
2024-05-22 08:59:17 +0000

    Create show_reviews view

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,12 @@ from .models import Review
 def index(request):
     latest_reviews = Review.objects.order_by('-created_on')[:3]
     return render(request, 'blog/index.html', {'latest_reviews': latest_reviews})
+
+# To show all reviews as paginated list, 6 per page.
+def show_reviews(request):
+    reviews_list = Review.object.all()
+    paginator = Paginator(reviews_list, 6)
+    page_number = request.GET.get('page')
+    reviews_page = paginator.get_page(page_number)
+
+    return render(request, 'show_reviews.html', {'reviews.page': reviews_page})
