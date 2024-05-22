871bdc158abcf38bd58bef221885117a53db6a74
Olle Brask <ollstah@gmail.com>
2024-05-22 09:41:55 +0000

    Add content to show_reviews

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('reviews/', views.show_reviews, name='show_reviews')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.core.paginator import Paginator
 from .models import Review
 
 # Create your views here.
@@ -9,9 +9,9 @@ def index(request):
 
 # To show all reviews as paginated list, 6 per page.
 def show_reviews(request):
-    reviews_list = Review.object.all()
+    reviews_list = Review.objects.all()
     paginator = Paginator(reviews_list, 6)
     page_number = request.GET.get('page')
     reviews_page = paginator.get_page(page_number)
 
-    return render(request, 'show_reviews.html', {'reviews.page': reviews_page})
+    return render(request, 'blog/show_reviews.html', {'reviews_page': reviews_page})
