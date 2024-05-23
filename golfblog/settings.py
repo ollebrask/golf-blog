620d7a9fee14bde39cbc699cd723edb3f462db99
Olle Brask <ollstah@gmail.com>
2024-05-23 07:38:53 +0000

    Add secret key to env.py

--- golfblog/settings.py
+++ golfblog/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4d2#&140#f5u9ny$fus!p!5)d1%$1*_^$5lwukuw+w^pb1)x3o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
