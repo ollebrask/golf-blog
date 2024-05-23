97b3cba034839c55c8d86bb25546ff57b0fae682
Olle Brask <ollstah@gmail.com>
2024-05-23 07:49:52 +0000

    Add html for add_review

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('reviews/', views.show_reviews, name='show_reviews'),
     path('review/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('review/add/', views.add_review, name='add_review'),
 ]
