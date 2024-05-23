51bc7011eecba751787e32ebd9600b6f36d47473
Olle Brask <ollstah@gmail.com>
2024-05-23 11:28:34 +0000

    Add path for delete review

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('review/<int:review_id>/', views.review_detail, name='review_detail'),
     path('review/add/', views.add_review, name='add_review'),
     path('review/edit/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('review/delete/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
