7e3845bd0a9ab26fac25f35fc4012b3ba4ac53fb
Olle Brask <ollstah@gmail.com>
2024-05-24 13:07:09 +0000

    Add rating choices as a dropdown list

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import Review, Comment
 
 class ReviewForm(forms.ModelForm):
+    RATING_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    
+    rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select())
+    
     class Meta:
         model = Review
         fields = ['title', 'golf_course', 'content', 'rating']
